2983ed3772fc0c78894818102197106571051696
Rene Pickhardt <rpickhardt@gmail.com>
2018-08-25 01:04:59 +0200

    cleaned up code and added qr code for invoices

--- startServer.py
+++ startServer.py
@@ -5,6 +5,10 @@ Created on 20.08.2018
 
 derived from: https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
 
+#FIXME add dependancies to setup.py
+pip3.6 install qrcode[pil]
+pip3.6 install pylightning
+
 '''
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
@@ -13,24 +17,61 @@ from lightning.lightning import LightningRpc
 import time
 import json
 
-#FIXME: move this to the __init__  call and remove global statements
-rpc_interface = None
+
+import qrcode
+import base64
+from io import BytesIO
 
 class HackALappHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-    def __count_votes(self):
-        global rpc_interface
-        if rpc_interface == None:
-            rpc_interface = LightningRpc("/Users/rpickhardt/.lightning/lightning-rpc") 
+    global rpc_interface
+    global topic_dict
+    
+    rpc_interface = LightningRpc("/Users/rpickhardt/.lightning/lightning-rpc") 
+    
+    f = open("topics.txt")
+    topic_dict= {}
+    for line in f:
+        hash_id = hashlib.sha256(line.encode()).hexdigest()
+        topic_dict[hash_id] = line[:-1]
+    
+    def __footer(self):
+        return """<p>Want to learn more about this LAPP?  
+        Check out <a href="https://youtu.be/HXVDwRnU7_I">my youtube video about Hack A Lapp</a> <br><br>
+        You can also fund a payment channel with my <a href="https://youtu.be/HXVDwRnU7_I">lightning node</a> or 
+         or <a href="https://twitter.com/renepickhardt">follow me on twitter</a>
+                 or visit my <a href="https://www.rene-pickhardt.de">my personal Website</a></p>"""
+        
+    def __make_base64_qr_code(self,bolt11):
+        qr = qrcode.QRCode(
+            version = 1,
+            error_correction = qrcode.constants.ERROR_CORRECT_H,
+            box_size = 4,
+            border = 4,
+        )
+        
+        qr.add_data(bolt11)
+        qr.make(fit=True)
+        img = qr.make_image()
+        
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")        
+        return img_str
+    
+    def __add__invoice_payment_string(self,invoice):
+        #FIXME: check for expired incoices and generate a new
 
-        invoices = rpc_interface.listinvoices()["invoices"]
+        message ="""<h2>Look up <a href="/"""+invoice["payment_hash"]+"""\">the results </a> (only possible after you paid the invoice)</h2>"""
         
-        #FIXME: move this to a class property and away from this function
-        f = open("topics.txt")
-        topic_dict= {}
-        for line in f:
-            hash_id = hashlib.sha256(line.encode()).hexdigest()
-            topic_dict[hash_id] = line[:-1]
-        
+        message += "<p>Each vote costs 250 Satoshi. Please pay " + str(int(invoice["msatoshi"])/1000) + " via the following invoice: </p>"
+                        
+        message += invoice["bolt11"]
+        message +="<br><br><img src=\"data:image/png;base64," + self.__make_base64_qr_code(invoice["bolt11"]) + "\">"
+
+        return message
+    
+    def __count_votes(self):
+        invoices = rpc_interface.listinvoices()["invoices"]        
         count_votes = {}
         
         for invoice in invoices: 
@@ -52,32 +93,21 @@ class HackALappHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         return count_votes
     
     def __show_start_page(self):
-        f = open("topics.txt")
         message = """<form method="post"><fieldset><legend>Which Videos would you like to have on my 
         <a href="https://www.youtube.com/channel/UCWmagvXbbOAS29CcLRP0FWg">Youtube Channel</a> and on
          <a href="https://en.wikiversity.org/wiki/Lightning_Network">Wikiversity</a>?</legend>"""
         # shuffle lines
-        for line in f:
-            hash_id = hashlib.sha256(line.encode()).hexdigest()
+        for hash_id, topic in topic_dict.items():
             message += """<div> <input type="checkbox" id=\"""" + hash_id + """\" name="votes"
-               value=\"""" + hash_id + """\" /> <label for=\"""" + hash_id + """\"">""" + line + "</label></div>"
+               value=\"""" + hash_id + """\" /> <label for=\"""" + hash_id + """\"">""" + topic + "</label></div>"
         message += """</fieldset><div><button type="submit">Vote now!</button></div></form>"""
         return message
     
-    def __show_rankings_page(self):
-        global rpc_interface
-        if self.path == "/favicon.ico":
-            return ""
-        
-        f = open("topics.txt")
-        topic_dict= {}
-        for line in f:
-            hash_id = hashlib.sha256(line.encode()).hexdigest()
-            topic_dict[hash_id] = line[:-1]
+    def __show_rankings_page(self,payment_hash):
         message = ""
-        #pay attention the self.path variable might have to be escaped
+        num_votes = 0
         try:
-            f = open("potentialVotes"+self.path+".json", "r")
+            f = open("potentialVotes/"+payment_hash+".json", "r")
             data = json.load(f)
             votes = data["v"]
             message += "<h1>You have voted for:</h1>"
@@ -88,10 +118,10 @@ class HackALappHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             pass
             
         invoices = rpc_interface.listinvoices()["invoices"]
-        message +="<h2>The current rankings of the poll are: </h2>"
         for invoice in invoices: 
-            if invoice["payment_hash"] == self.path[1:]:
+            if invoice["payment_hash"] == payment_hash: #self.path[1:]:
                 if invoice["status"]=="paid":
+                    message +="<h2>The current rankings of the poll are: </h2>"
                     message+="great you paid your fair share! The ranking will be impolemented soon"
                     message+="<ul>"
                     for key, count in self.__count_votes().items():
@@ -99,18 +129,12 @@ class HackALappHTTPServer_RequestHandler(BaseHTTPRequestHandler):
                     message+="</ul>"
 
                 else:
-                    #FIXME: check for expired incoices and generate a new
-                    message+="Please pay your invoice first! The invoice is: <br><br>"
-                    message+=invoice["bolt11"]
+                    message += self.__add__invoice_payment_string(invoice)
         
         return message
 
     def do_GET(self):
-        global rpc_interface
-        if rpc_interface == None:
-            rpc_interface = LightningRpc("/Users/rpickhardt/.lightning/lightning-rpc") 
         self.send_response(200)
-        
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         
@@ -118,56 +142,39 @@ class HackALappHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         if self.path == "/":
             message = self.__show_start_page()
         else:
-            message = self.__show_rankings_page()
-                    
+            if self.path != "/favicon.ico":
+                message = self.__show_rankings_page(self.path[1:])
+        
+        message += self.__footer()
+           
         self.wfile.write(bytes(message, "utf8"))
         return
     
     def do_POST(self):
-        global rpc_interface
-        if rpc_interface == None:
-            rpc_interface = LightningRpc("/Users/rpickhardt/.lightning/lightning-rpc") 
         self.send_response(200)
-        
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         
         length = int(self.headers.get('content-length'))
         field_data = self.rfile.read(length)
-        if self.path=="/payment_requests":
-            amount = json.loads(field_data.decode("utf-8"))["amount"]
-            label = "satoshis-tennis-{}".format(time.time())
-            message = rpc_interface.invoice(amount*1000, label , label, fallbacks=["3MXqbMwf457U4Jaw35WnJdnL99mq7Q8oQQ"])["bolt11"]
-            rpc_interface.waitinvoice(label)
-            self.wfile.write(bytes(message, "utf8"))
-            return
         fields = parse_qs(field_data)
-        print(fields)
-        votes = [s.decode('ascii') for s in fields[b"votes"]]
         
-        message = "received your " + str(len(votes)) + "votes! You have voted for the following questions: </p>"
-        message += "<ul>"
-        f = open("topics.txt")
-        for line in f:
-            hash_id = hashlib.sha256(line.encode()).hexdigest()
-            if hash_id in votes:
-                message += "<li>" + line + "</li>"
-        message += "</ul>"
-        
-        amount = 250 * len(votes) * 1000
-        
-        #l = LightningRpc("/Users/rpickhardt/.lightning/lightning-rpc")
-        now = time.time()
-        invoice = rpc_interface.invoice(amount,"lbl{}".format(now),"rene-pickhardts-lightning-video-votes-{}".format(now))
-        
-        message += "Each vote costs 250 Satoshi please pay: " + str(amount/1000) + " via the following invoice: <br>"
-        message +="<h1>You need to pay the invoice in order to look up the results</h1>"
-        message +=""" Look up <a href="/"""+invoice["payment_hash"]+"""\">the results </a> (only possible after you paid your invoice) """
-        message += invoice["bolt11"]
-        
-        dict = {"v":votes, "i":invoice}
-        with open("potentialVotes/"+invoice["payment_hash"]+".json","w") as outfile:
-            json.dump(dict,outfile)
+        message =""
+        if b"votes" not in fields:
+            message = "malformed post request"
+        else: 
+            votes = [s.decode('ascii') for s in fields[b"votes"]]            
+            amount = 250 * len(votes) * 1000
+            now = time.time()
+            
+            invoice = rpc_interface.invoice(amount,"lbl{}".format(now),"rene-pickhardts-lightning-video-votes-{}".format(now))
+
+            data_dict = {"v":votes, "i":invoice}
+            with open("potentialVotes/"+invoice["payment_hash"]+".json","w") as outfile:
+                json.dump(data_dict,outfile)
+
+            message += self.__show_rankings_page(invoice["payment_hash"])
+        message += self.__footer()
         
         self.wfile.write(bytes(message, "utf8"))
         return
